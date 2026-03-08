8ef0ca8a595e8a725ba08d82b2cf596163c07776
mtszc <mtszccr@gmail.com>
2026-03-08 17:53:55 +0100

    Switch to use postgres instead of sqlite

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
-from os import environ
+from dotenv import load_dotenv
 from db import db
 from blocklist import BLOCKLIST
 from resources.item import blp as ItemBlueprint
@@ -12,6 +13,7 @@ from flask_migrate import Migrate
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
@@ -20,14 +22,14 @@ def create_app(db_url=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
     migrate = Migrate(app, db)
     api = Api(app)
 
-    app.config["JWT_SECRET_KEY"] = 'TODO' #environ["JWT_SECRET_KEY"]
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     jwt = JWTManager(app)
 
     @jwt.token_in_blocklist_loader
